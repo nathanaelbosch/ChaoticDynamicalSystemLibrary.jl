727ddcbfdfafb93b97422b76c6478cf5a5bd1d57
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2023-12-23 12:03:08 +0100

    Test that all the defined systems work and are solvable

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,12 @@
 using ChaoticDynamicalSystemLibrary
 using Test
+using SafeTestsets
 using Aqua, JET
 
 @testset "ChaoticDynamicalSystemLibrary.jl" begin
     # Write your tests here.
+
+    include("test_chaotic_attractors.jl")
 
     @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(ChaoticDynamicalSystemLibrary;
